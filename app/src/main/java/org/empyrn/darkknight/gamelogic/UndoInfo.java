f37357c33f86104b925d12a604e45e0dda174d17
--unset <--unset>
2018-11-30 15:11:09 +0900

    UndoInfo code review
    Signed-off-by: --unset <--unset>

--- app/src/main/java/org/empyrn/darkknight/gamelogic/UndoInfo.java
+++ app/src/main/java/org/empyrn/darkknight/gamelogic/UndoInfo.java
@@ -5,6 +5,13 @@ package org.empyrn.darkknight.gamelogic;
  * Set by makeMove(). Used by unMakeMove().
  * @author petero
  */
+
+/*
+이전의 상태로 되돌려 주는 함수
+이전의 이동상태 정보를 가지고 있음
+unMakeMove()함수를 사용했을 때 이전의 상태로 되돌아감
+ */
+
 public class UndoInfo {
     int capturedPiece;
     int castleMask;
