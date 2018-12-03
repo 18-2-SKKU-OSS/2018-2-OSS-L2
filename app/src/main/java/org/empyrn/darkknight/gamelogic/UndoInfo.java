ca7a2519bdc4d4d522a6388e387183b67a9993bf
hyunbeen <bigpig93@naver.com>
2018-12-04 03:25:15 +0900

    add UndoInfo.java in korean

--- app/src/main/java/org/empyrn/darkknight/gamelogic/UndoInfo.java
+++ app/src/main/java/org/empyrn/darkknight/gamelogic/UndoInfo.java
@@ -1,9 +1,14 @@
 package org.empyrn.darkknight.gamelogic;
 
 /**
+ * English
  * Contains enough information to undo a previous move.
  * Set by makeMove(). Used by unMakeMove().
  * @author petero
+ * korean
+ * 이전 이동을 취소 할 수있는 충분한 정보가 들어 있습니다.
+ * makeMove ()에 의해 설정됩니다. unMakeMove ()에 의해 사용됩니다.
+ * 저자 petero
  */
 public class UndoInfo {
     int capturedPiece;
