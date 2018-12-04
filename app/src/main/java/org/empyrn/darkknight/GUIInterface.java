6e814691f1131ca04572ea64e6ce9be4ef154813
parkg17 <parkg17@naver.com>
2018-12-04 22:23:56 +0900

    add GUIInterface.java in Korean

--- app/src/main/java/org/empyrn/darkknight/GUIInterface.java
+++ app/src/main/java/org/empyrn/darkknight/GUIInterface.java
@@ -32,6 +32,7 @@ public interface GUIInterface {
 	public void setThinkingInfo(String pvStr, String bookInfo, List<Move> pvMoves, List<Move> bookMoves);
 	
 	/** Ask what to promote a pawn to. Should call reportPromotePiece() when done. */
+    /** 폰을 무슨 말로 바꿀지 물어본다. 끝나면 reportPromotePiece() 함수를 실행한다. */
 	public void requestPromotePiece();
 
 	/** Run code on the GUI thread. */
@@ -39,12 +40,15 @@ public interface GUIInterface {
 	public void runOnUIThread(Runnable runnable);
 
 	/** Report that user attempted to make an invalid move. */
+    /** 사용자가 유효하지 않은 move를 시도하였다고 알린다. */
 	public void reportInvalidMove(Move m);
 
 	/** Called when computer made a move. GUI can notify user, for example by playing a sound. */
+    /** 컴퓨터(AI)가 move를 했을 때 부르는 함수. GUI는 사용자에게 알릴 수 있습니다. (예를 들어 소리를 재생하여서) */
 	public void computerMoveMade();
 	public void humanMoveMade(Move m);
 
 	/** Report remaining thinking time to GUI. */
+    /** 남아있는 thinking 시간을 GUI에게 알린다. */
 	public void setRemainingTime(long wTime, long bTime, long nextUpdate);
 }
