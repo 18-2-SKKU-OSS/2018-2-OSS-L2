a9d8472c291d66ba6bb593c221e67c5457867ffd
HyeonCheol Yun <30384444+hotgake@users.noreply.github.com>
2018-12-10 14:25:19 +0900

    C++ STL의 Pair 와 동일한 형태를 갖는 헬퍼클래스
    주석 수정 및 추가, 파일설명 설정

--- app/src/main/java/org/empyrn/darkknight/gamelogic/Pair.java
+++ app/src/main/java/org/empyrn/darkknight/gamelogic/Pair.java
@@ -4,6 +4,9 @@ package org.empyrn.darkknight.gamelogic;
  * A small helper class that makes it possible to return two values from a function.
  * @author petero
  */
+
+// Pair 형태를 다루기 위한 헬퍼 템플릿 클래스.
+// C++ STL의 Pair 객체와 완전히 동일함.
 public final class Pair<T1, T2> {
     public final T1 first;
     public final T2 second;
