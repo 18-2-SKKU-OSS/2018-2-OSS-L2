7c9fcb3a7a6adf6baacdd52e61c1717fd5177c90
hyunbeen <bigpig93@naver.com>
2018-12-10 14:09:38 +0900

    file information

--- gradlew.bat
+++ gradlew.bat
@@ -1,7 +1,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  Gradle startup script for Windows 
 @rem
 @rem ##########################################################################
 
@@ -11,7 +11,7 @@ if "%OS%"=="Windows_NT" setlocal
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
-set DIRNAME=%~dp0
+set DIRNAME=%~dp0   
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
@@ -36,7 +36,7 @@ set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
-
+  
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
