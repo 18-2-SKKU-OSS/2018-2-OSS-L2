695231f9500f9264061eac96b2c0f53ed5d245fd
hyunbeen <bigpig93@naver.com>
2018-12-11 00:57:20 +0900

    add ChessParseError.java comment in korean

--- app/src/main/java/org/empyrn/darkknight/gamelogic/ChessParseError.java
+++ app/src/main/java/org/empyrn/darkknight/gamelogic/ChessParseError.java
@@ -4,6 +4,10 @@ package org.empyrn.darkknight.gamelogic;
  * Exception class to represent parse errors in FEN or algebraic notation.
  * @author petero
  */
+/**
+  * FEN 또는 대수 표기법의 구문 분석 오류를 나타내는 예외 클래스입니다.
+  * @ petero
+*/
 public class ChessParseError extends Exception {
 	private static final long serialVersionUID = -6051856171275301175L;
 	
